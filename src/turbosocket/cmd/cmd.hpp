b666711a5a2ad2934f934e89b1fea2fd93deadc7
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-11-25 22:44:22 +0000

    [wip] TS cmd, onetime object

---
+++ src/turbosocket/cmd/cmd.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "libs1.hpp"
+
--- src/turbosocket/turbosocket.hpp
+++ src/turbosocket/turbosocket.hpp
@@ -5,7 +5,11 @@
 
 namespace n_turbosocket {
 
+class c_turbosocket_system_pimpl;
+
 class c_turbosocket_system {
+	protected:
+		unique_ptr<c_turbosocket_system_pimpl> m_pimpl;
 	public:
 		c_turbosocket_system();
 		virtual ~c_turbosocket_system();
